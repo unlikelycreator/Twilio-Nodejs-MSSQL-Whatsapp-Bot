f7207fa5f67d20f0e9f75f2cd83c8ca54c8e703e
Hritik Pawar <73632558+unlikelycreator@users.noreply.github.com>
2023-07-13 13:35:21 +0530

    Update nodeService.js

--- nodeService.js
+++ nodeService.js
@@ -1,9 +1,9 @@
 const Service = require('node-windows').Service;
 
 const svc = new Service({
-    name: "WhatsappBot",
-    description: "Whatsapp Message Sending Bot",
-    script: "E:\\react-mssql\\Tasker-authentication\\WhatsappBot\\app.js"
+    name: "WhatsappBot",   //Edit This
+    description: "Whatsapp Message Sending Bot",    //Edit This
+    script: "E:\\react-mssql\\Tasker-authentication\\WhatsappBot\\app.js"    //Edit This
 })
 
 svc.on('install', function(){
